43c41386cbd4296b26b976e46b91d3bb67d7f6ef
19013219_kim <cyworld5057@naver.com>
2025-04-10 13:17:48 +0900

    style:피부질환 사진업로드 디자인1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        scroll: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        scroll: "scroll 50s linear infinite",
+      },
+    },
   },
   plugins: [],
 };
