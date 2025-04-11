3bdf9fce785ed0e877d1dcc291fbc06e164ec842
19013219_kim <cyworld5057@naver.com>
2025-04-11 14:19:10 +0900

    feat:Main화면에 피부피묭 추가

--- src/app/(provider)/(root)/page.tsx
+++ src/app/(provider)/(root)/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import MainDiagnose from "./components/MainDiagnose";
+import MainSkinDiagnose from "./components/MainSkinDiagnose";
 
 export default function Home() {
   return (
     <div className="w-full flex flex-col">
       <MainDiagnose />
+      <MainSkinDiagnose />
     </div>
   );
 }
