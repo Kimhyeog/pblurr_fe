ccadcf36cc52e64147d0e615589c6c0e2c4817dd
19013219_kim <cyworld5057@naver.com>
2025-05-07 17:38:39 +0900

    feat: 스킨케어미션 모바일 반응형 수정

--- src/app/(provider)/(root)/skincaremission/components/ScoreBoard/ScoreRank123.tsx
+++ src/app/(provider)/(root)/skincaremission/components/ScoreBoard/ScoreRank123.tsx
@@ -30,22 +30,37 @@ function ScoreRankBox() {
   const rankHeights = ["h-40", "h-32", "h-24"]; // 1등이 가장 높음
 
   return (
-    <div className="w-full h-full bg-white border-4 border-[#5CA7C8] p-6 rounded-xl shadow-md max-w-xl mx-auto">
-      <h2 className="text-2xl font-bold text-center mb-6 text-sky-700">
+    <div className="w-full h-full bg-white border-4 border-[#5CA7C8] p-4 sm:p-6 rounded-xl shadow-md  sm:max-w-xl mx-auto">
+      <h2 className="text-md sm:text-2xl font-bold text-center mb-4 sm:mb-6 text-sky-700">
         🏆 미션 점수 상위 3명
       </h2>
-      <div className="flex justify-center items-end gap-4">
+      <div className="flex justify-center items-end gap-3 sm:gap-4">
         {topUsers.map((user, index) => (
           <motion.div
             key={user.userId}
-            className={`w-24 ${rankHeights[index]} ${rankColors[index]} rounded-t-xl flex flex-col justify-end items-center text-white shadow-lg`}
+            className={`w-20 sm:w-24 
+                      ${
+                        index === 0
+                          ? "h-32 sm:h-40"
+                          : index === 1
+                          ? "h-28 sm:h-32"
+                          : "h-24 sm:h-28"
+                      } 
+                      ${rankColors[index]} 
+                      rounded-t-xl flex flex-col justify-end items-center text-white shadow-lg`}
             initial={{ y: 50, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5, delay: index * 0.2 }}
           >
-            <span className="text-lg font-semibold mt-2">{index + 1}등</span>
-            <span className="text-sm mb-2">{user.userId}</span>
-            <span className="text-xs mb-4">점수: {user.totalScore}</span>
+            <span className="text-sm sm:text-lg font-semibold mt-2">
+              {index + 1}등
+            </span>
+            <span className="text-xs sm:text-sm mb-1 sm:mb-2">
+              {user.userId}
+            </span>
+            <span className="text-[10px] sm:text-xs mb-3 sm:mb-4">
+              점수: {user.totalScore}
+            </span>
           </motion.div>
         ))}
       </div>
